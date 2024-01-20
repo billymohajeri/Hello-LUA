8f2f63bc591dcc028920e7de2bb961a9ef81ebf6
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-01-20 13:13:22 +0200

    Try for loop

--- Hello.lua
+++ Hello.lua
@@ -1,8 +1,15 @@
-print ("Just another Hello World!")
+print("Just another Hello World!")
 
 myName = "Billy"
-print (myName)
+print(myName)
 
 -- trying comments
 
 print (type(myName))
+
+for i = 1, 10 do
+    for j = 1, 10 do
+        io.write(i * j, " ")
+    end
+    print()
+end
